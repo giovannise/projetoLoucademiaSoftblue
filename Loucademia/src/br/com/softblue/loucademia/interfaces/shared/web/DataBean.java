6d4cb2c72331e290408062684065b38c9b6c9728
Giovannise <Giovannise@192.168.15.5>
2019-06-21 19:57:49 -0300

    Início da implementação de EJB Inserindo os estados no banco para exibir no combobox.

--- Loucademia/src/br/com/softblue/loucademia/interfaces/shared/web/DataBean.java
+++ Loucademia/src/br/com/softblue/loucademia/interfaces/shared/web/DataBean.java
@@ -1,31 +1,36 @@
-package br.com.softblue.loucademia.interfaces.web;
+package br.com.softblue.loucademia.interfaces.shared.web;
 
 import java.io.Serializable;
 import java.util.List;
 
+import javax.activation.DataSource;
+import javax.ejb.EJB;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
-
-
+import br.com.softblue.loucademia.application.service.DataService;
 import br.com.softblue.loucademia.domain.aluno.Aluno.Sexo;
 import br.com.softblue.loucademia.domain.aluno.Aluno.Situacao;
 import br.com.softblue.loucademia.domain.aluno.Estado;
+import br.com.softblue.loucademia.domain.aluno.EstadoRepository;
 
 @Named
 @ApplicationScoped
 public class DataBean implements Serializable {
 
+	@EJB
+	private DataService dataService;
+	
 	public Sexo[] getSexos() {
-		return Sexo.values();
+		return dataService.getSexos();
 	}
 	
 	public Situacao[] getSituacoes() {
-		return Situacao.values();
+		return dataService.getSituacoes();
 	}
 	
 	public List<Estado> getEstados() {
-		return null;
+		return dataService.listEstados();
 	}
 
 }
